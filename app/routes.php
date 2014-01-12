b2afaf6606a9e384a374e253bd78613e7bf98e0e
Mulia Arifandy Nasution <mul14.net@gmail.com>
2014-01-12 16:28:55 +0700

    Keduax

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,6 @@
 <?php
-
+Route::get('/', function(){
+    return Redirect::to('install');
+});
 Route::get('/install', 'InstallController@step');
 Route::post('/install', 'InstallController@setStep');
